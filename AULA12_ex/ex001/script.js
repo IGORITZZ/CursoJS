2db0a6afc12eb4ea9a09df0593e0c9aedd9965c2
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2023-12-01 12:02:49 -0300

    att img

--- AULA12_ex/ex001/script.js
+++ AULA12_ex/ex001/script.js
@@ -1,11 +1,22 @@
-var hora = new Date()
-var h = hora.getHours()
 
-var restxt = document.querySelector('#texthora')
-var img = document.querySelector('#imghora')
+function carregar(){
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#imagem')
+    var data = new Date()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
 
-
-    if ( h < 12){
-        restxt.innerHTML = `<p>A hora é ${h} horas. Bom dia!</p>`
-        img.innerHTML = `<img src=""`
+    if ( hora >= 0 && hora <= 12 ){
+        img.src = 'images/amanhecer.jpg'
+        document.body.style.background = 'lightblue'
+        document.querySelector('section').style.background = 'lightblue'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'images/tarde.jpeg'
+        document.body.style.background = 'rgb(254, 188, 161)'
+        document.querySelector('section').style.background = 'rgb(254, 188, 161)'
+    } else {
+        img.src = 'images/noite.jpg'
+        document.body.style.background = 'grey'
+        document.querySelector('section').style.background = 'grey'
     }
+}
