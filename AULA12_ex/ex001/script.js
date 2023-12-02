51aa21f9c025e850e088070b2913a0f81fdbf11f
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2023-12-02 12:16:21 -0300

    Update script.js

--- AULA12_ex/ex001/script.js
+++ AULA12_ex/ex001/script.js
@@ -3,7 +3,7 @@ function carregar(){
     var msg = document.querySelector('#msg')
     var img = document.querySelector('#imagem')
     var data = new Date()
-    var hora = 20
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if ( hora >= 0 && hora <= 12 ){
@@ -11,12 +11,12 @@ function carregar(){
         document.body.style.background = 'lightblue'
         document.querySelector('section').style.background = 'lightblue'
     } else if (hora >= 12 && hora <= 18){
-        img.src = 'images/tarde.jpeg'
-        document.body.style.background = 'rgb(254, 188, 161)'
-        document.querySelector('section').style.background = 'rgb(254, 188, 161)'
+        img.src = 'images/tarde.jpg'
+        document.body.style.background = '#AD9B3E'
+        document.querySelector('section').style.background = '#AD9B3E'
     } else {
         img.src = 'images/noite.jpg'
-        document.body.style.background = 'grey'
-        document.querySelector('section').style.background = 'grey'
+        document.body.style.background = 'rgb(107, 165, 241)'
+        document.querySelector('section').style.background = 'rgb(107, 165, 241)'
     }
 }
