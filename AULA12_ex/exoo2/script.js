33025dec5c728e15042d909121170f3174526e39
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2023-12-02 12:16:32 -0300

    Update script.js

--- AULA12_ex/exoo2/script.js
+++ AULA12_ex/exoo2/script.js
@@ -4,9 +4,18 @@ var fano = document.querySelector('#txtano')
 var res = document.querySelector('#res')
 
 function verificar(){
-    if ( fano.value == 0 || fano.value > ano ){
+    if ( fano.value == 0 || Number(fano.value) > ano ){
         alert('[ERRO] Verfique as informações e tente novamente!')
     } else {
-        alert('Esta tudo certo!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if ( fsex[0].checked ){
+            genero = 'Homem'
+        } else if ( fsex[1].checked )
+        {
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
