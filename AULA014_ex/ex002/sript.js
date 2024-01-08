800adf7ecf020ec0dfd72217852ae8f0e69aba1b
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-01-08 11:08:49 -0300

    Update sript.js

--- AULA014_ex/ex002/sript.js
+++ AULA014_ex/ex002/sript.js
@@ -1,8 +1,10 @@
 function tabuada(){
     let num = document.querySelector('#txtn')
     let tab = document.querySelector('#seltab')
+
     if ( num.value.length == 0){
         alert('[ERRO] Digite um numero valido!')
+        
     } else {
         let n = Number(num.value)
         let c = 1
