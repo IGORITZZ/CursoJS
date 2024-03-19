402cacbfaad1e9eac9b7a55b574f1e6f451bbb3d
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-03-19 12:19:34 -0300

    fundamentos

---
+++ React - Udemy/2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+
+//components
+import FirstComponent from './components/FistComponent.jsx';
+
+//styles ou css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
