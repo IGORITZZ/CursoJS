e2e583b1faf0309041d6d37fde61248fdc5ae9b1
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-03-22 08:42:06 -0300

    desafio

--- React - Udemy/DESAFIOS/desafio-2/src/components/Challenge.jsx
+++ React - Udemy/DESAFIOS/desafio-2/src/components/Challenge.jsx
@@ -1,24 +1,20 @@
 const Challenge = () => {
 
+    const handleMyEvent = () => {
+        console.log('Ativou o evento?')
+    }
+
     return(
         <div>
             <div>
                 <h1>Contando no console</h1>
             </div>
             <div>
-                <input type="number" name="n1" id="n1"/>
-                <button onClick={contador()}>Contando</button>
+                <button onClick={handleMyEvent}>Clique aqui</button>
+
             </div>
         </div>
     )
 
 }
-
-const contador = () => {
-    let n1 =
-    let n2 = 
-    console.log(n1 + n2);
-    
-}
-
 export default Challenge
