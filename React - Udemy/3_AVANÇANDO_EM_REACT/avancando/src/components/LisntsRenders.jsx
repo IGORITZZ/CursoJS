154b3735df8eef1588b9d5c00197db3c1abf0e5e
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-03-25 11:57:47 -0300

    att

---
+++ React - Udemy/3_AVANÇANDO_EM_REACT/avancando/src/components/LisntsRenders.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+const LisntsRenders = () => {
+    const [list] = useState('Igor', 'Bruno', 'Thomaz')
+  return (
+    <div>
+        <ul>
+            {list.map((item) => (
+                <li>{item}</li>
+            ) )}
+        </ul>
+    </div>
+  )
+}
+
+export default LisntsRenders
